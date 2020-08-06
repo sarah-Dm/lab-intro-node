34b10e9c7adddca98d5d515900875e329b43cecd
trangnguyen <nttrang30792@gmail.com>
2020-08-06 21:33:12 +0200

    iteration 2345 ok

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 class SortedList {
   constructor() {
     this.items = [];
-    this.length = 0;
+    this.length = 0; //this.items.length
   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function (a,b) {
+      return a-b;
+    });
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos>=0 && pos < this.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+    
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.sort(function (a,b) {
+        return b-a;
+      })
+      return this.items[0]
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      this.items.sort(function (a,b) {
+        return a-b;
+      })
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
